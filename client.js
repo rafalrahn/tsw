d7b31d8def894698d97fe56b063d5a2b58d2b8c3
Rafał Rahn <rafal@rahn.pl>
2013-06-04 11:30:06 +0200

    powoli zacznie to wyglądać

--- client.js
+++ client.js
@@ -1,3 +1,15 @@
-function() {
+(function() {
   if (window["WebSocket"]) {
     $(document).ready(function() {
+      var animacja, canvas, polaczenie, context, id, sendDirection, server;
+      server = null;
+      canvas = $("#poziom");
+      context = canvas.get(0).getContext("2d");    
+      id = null;
+      sendDirection = function(direction) {
+        if (server) {
+          return server.send(JSON.stringify({
+            'direction': direction
+          }));
+        }
+      };
