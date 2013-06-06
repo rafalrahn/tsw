d619396e033d26166332c51c68540eec1df33a19
Rafał Rahn <rafal@rahn.pl>
2013-06-06 09:57:26 +0200

    gotowe

--- server.js
+++ server.js
@@ -84,3 +84,129 @@
       }
       return this.moveHead();
     };
+    Snake.prototype.moveTail = function(i) {//zwiększa głowe zmniejsza ogon
+      this.elements[i][0] = this.elements[i + 1][0];
+      return this.elements[i][1] = this.elements[i + 1][1];
+    };
+    Snake.prototype.moveHead = function() {
+      var head;
+      head = this.length - 1;
+      switch (this.direction) {
+        case "left":
+          this.elements[head][0] -= 1;
+          break;
+        case "right":
+          this.elements[head][0] += 1;
+          break;
+        case "up":
+          this.elements[head][1] -= 1;
+          break;
+        case "down":
+          this.elements[head][1] += 1;
+      }
+      if (this.elements[head][0] < 0) {
+        this.elements[head][0] = STAGE_WIDTH;
+      }
+      if (this.elements[head][1] < 0) {
+        this.elements[head][1] = STAGE_HEIGHT;
+      }
+      if (this.elements[head][0] > STAGE_WIDTH) {
+        this.elements[head][0] = 0;
+      }
+      if (this.elements[head][1] > STAGE_HEIGHT) {
+        return this.elements[head][1] = 0;
+      }
+    };
+    Snake.prototype.head = function() {
+      return this.elements[this.length - 1];
+    };
+    Snake.prototype.blocks = function(other) { //blokowanie innego gracza
+      var collision, element, head, _i, _len, _ref;
+      head = other.head();
+      collision = false;
+      _ref = this.elements;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        element = _ref[_i];
+        if (head[0] === element[0] && head[1] === element[1]) {
+          collision = true;
+        }
+      }
+      return collision;
+    };
+    Snake.prototype.blocksSelf = function() { //blokowanie siebie
+      var collision, head, i, _ref;
+      head = this.head();
+      collision = false;
+      for (i = 0, _ref = this.length - 2; 0 <= _ref ? i <= _ref : i >= _ref; 0 <= _ref ? i++ : i--) {
+        if (head[0] === this.elements[i][0] && head[1] === this.elements[i][1]) {
+          collision = true;
+        }
+      }
+      return collision;
+    };
+    return Snake;
+  })();
+  /* Handle Connections */
+  socket = io.listen(server);
+  socket.on("connection", function(client) {
+    var clientId, clientSnake;
+    clientId = autoClient;
+    clientSnake = new Snake(clientId);
+    autoClient += 1;
+    snakes.push(clientSnake);
+    sys.puts("Client " + clientId + " connected");
+    client.send(JSON.stringify({
+      type: 'id',
+      value: clientId
+    }));
+    client.on("message", function(message) {
+      message = JSON.parse(message);
+      return clientSnake.direction = message.direction;
+    });
+    return client.on("disconnect", function() {
+      snakes.remove(clientSnake);
+      return sys.puts("Client " + clientId + " disconnected");
+    });
+  });
+  /* Update Game State */
+  updateState = function() {
+    var snake, _i, _len;
+    for (_i = 0, _len = snakes.length; _i < _len; _i++) {
+      snake = snakes[_i];
+      snake.doStep();
+    }
+    checkCollisions();
+    return socket.broadcast(JSON.stringify({
+      type: 'snakes',
+      value: snakes
+    }));
+  };
+  checkCollisions = function() {
+    var other, resetSnakes, snake, _i, _j, _k, _len, _len2, _len3, _results;
+    resetSnakes = [];
+    for (_i = 0, _len = snakes.length; _i < _len; _i++) {
+      snake = snakes[_i];
+      if (snake.blocksSelf()) {
+        resetSnakes.push(snake);
+      }
+      for (_j = 0, _len2 = snakes.length; _j < _len2; _j++) {
+        other = snakes[_j];
+        if (other !== snake) {
+          if (other.blocks(snake)) {
+            resetSnakes.push(snake);
+            other.addKill();
+          }
+        }
+      }
+    }
+    _results = [];
+    for (_k = 0, _len3 = resetSnakes.length; _k < _len3; _k++) {
+      snake = resetSnakes[_k];
+      _results.push(snake.reset());
+    }
+    return _results;
+  };
+  tick = setInterval(updateState, 100);
+  /* Start Server */
+  sys.puts("Server running on port " + port);
+}).call(this);
